b718d6784c14f83f8083c2ddee0cae2e8545016c
tommytnez <tommytnez@yahoo.com>
2017-01-30 11:39:30 -0800

    authroutertest

---
+++ test/auth-router-test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+require('./mock-env.js');
+const expect = require('chai').expect;
+const superagent = require('superagent');
+const User = require('../model/user.js');
+const userMocks = require('./lib/user-mocks.js');
+const serverControl = require('./lib/server-control.js');
+
+const baseURL = `http://localhost:${process.env.PORT}`;
+
+describe('testing auth-router', function(){
+  before(serverControl.startServer);
+  after(serverControl.killServer);
+  afterEach((done) => {
+    User.remove({})
+    .then(() => done())
+    .catch(done);
+  });
+
+  describe('testing POST /api/signup', function(){
+    it('should return a user', function(done){
+      superagent.post(`${baseURL}/api/signup`)
+      .send({
+        username: 'goaltastic',
+        email: 'goaltastic@goaltastic.com',
+        password: '1234',
+        phone: '5555555555',
+      })
+      .then(res => {
+        console.log('token: ', res.text);
+        expect(res.status).to.equal(200);
+        expect(Boolean(res.text)).to.equal(true);
+        done();
+      })
+      .catch(done);
+    });
+  });
+
+  describe('testing GET /api/login', function(){
+    before(userMocks.bind(this));
+
+    it('should respond with a token', (done) => {
+      superagent.get(`${baseURL}/api/login`)
+      .auth(this.tempUser.username, '1234')
+      .then(res => {
+        expect(res.status).to.equal(200);
+        expect(Boolean(res.text)).to.equal(true);
+        done();
+      })
+      .catch(done);
+    });
+  });
+});
