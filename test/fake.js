a91d1e0150b680d842ee1dffa8b8064649d319b4
devonhackley <devonhackley@gmail.com>
2017-01-30 09:59:44 -0800

    commiting setup files

---
+++ test/fake.js
@@ -0,0 +1,8 @@
+'use strict';
+const expect = require('chai').expect;
+
+describe('this is a test', function(){
+  it('should be up and running', () => {
+    expect(true).to.equal(true);
+  });
+});
