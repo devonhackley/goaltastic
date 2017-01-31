1fb05fddc6a235d20055aef10f366e2bf224c6df
Geno Guerrero <geno.guerrero@gmail.com>
2017-01-31 14:43:42 -0800

    working in progress on profile, model and route completed, no tests yet.

--- test/lib/goal-mocks.js
+++ test/lib/goal-mocks.js
@@ -9,7 +9,6 @@ module.exports = function(done){
     title:  'test' + Math.floor(Math.random() * 100),
     start: Date.now(),
     userID: this.tempUser._id.toString(),
-    // milestoneID: this.tempMilestone._id.toString(),
   }).save()
   .then(goal => {
     this.tempGoal = goal;
