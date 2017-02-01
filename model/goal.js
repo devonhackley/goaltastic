f31eae82792e5a5ce8d8fe253f5d95d1cc1a6f74
tommytnez <tommytnez@yahoo.com>
2017-01-31 17:34:30 -0800

    hope

--- model/goal.js
+++ model/goal.js
@@ -7,7 +7,7 @@ const goalSchema = mongoose.Schema({
   title: {type:String, required:true},
   start:{type:Date, default: Date.now, required:true},
   userID: {type: mongoose.Schema.Types.ObjectId, required: true},
-  tasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'task'}],
+  taskID: [{type: mongoose.Schema.Types.ObjectId, ref: 'task'}],
 });
 
 module.exports = mongoose.model('goal', goalSchema);
